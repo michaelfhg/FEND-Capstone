b54009ef8c53f42eddfba73aeef1d80cc204de89
michaelfhg <43891677+michaelfhg@users.noreply.github.com>
2020-02-19 03:04:18 +0200

    Delete linkChecker.test.js

--- src/client/js/linkChecker.test.js
+++
@@ -1,9 +0,0 @@
-//import {chkLink} from './linkChecker.js';
-//import 'babel-polyfill';
-//import 'regenerator-runtime/runtime';
-//import 'core-js/modules/es6.promise';
-const regeneratorRuntime = require("regenerator-runtime");
-const chkLink = require('./linkChecker');
-test('Function chkLink should be defined', async() => {
-        expect(chkLink).toBeDefined();
-    });
